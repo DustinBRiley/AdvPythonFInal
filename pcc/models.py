5687e86da905b43265db140a38fef1b9f64a2f20
CrowRoth <dustinrileywork@gmail.com>
2023-12-12 17:46:20 -0700

    got model working

--- pcc/models.py
+++ pcc/models.py
@@ -6,8 +6,8 @@ from django.utils import timezone
 class PokemonCard(models.Model):
     creator = User.get_username
     creation_date = models.DateTimeField("date created")
-    Name = models.CharField(max_length=200)
-    Types = [
+    name = models.CharField(max_length=20)
+    types = [
         ("Normal", "Normal"),
         ("Electric", "Electric"),
         ("Grass", "Grass"),
@@ -16,8 +16,12 @@ class PokemonCard(models.Model):
         ("Fighting", "Fighting"),
         ("Psychic", "Psychic")
     ]
-    Type = models.CharField(max_length=8, choices=Types)
+    type = models.CharField(max_length=8, choices=types, default="Grass")
     image = models.ImageField(upload_to='images/')
+    move_name = models.CharField(max_length=20, default="Absorb")
+    move_text = models.CharField(max_length=200, default="Remove 2 damage counters")
+    move_dmg = models.IntegerField(default=20)
+
     def __str__(self):
         return self.Name
     def was_created_recently(self):
--- pcc/views.py
+++ pcc/views.py
@@ -1,10 +1,66 @@
-
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.urls import reverse
+from django.views import generic
 
 from .models import PokemonCard
 
+class IndexView(generic.ListView):
+    template_name = "pcc/index.html"
+    context_object_name = "latest_pokemon_list"
 
-def index(request):
-    latest_pokemoncard_list = PokemonCard.objects.order_by("-pub_date")[:5]
-    output = ", ".join([p.Name for p in latest_pokemoncard_list])
-    return HttpResponse(output)
+    def get_queryset(self):
+        """Return the last five created pokemon cards."""
+        return PokemonCard.objects.order_by("-creation_date")[:5]
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('collection.html')
+        else:
+            messages.info(request, 'Invalid Username or Password')
+            return redirect("login.html")
+    else:
+        return render(request, "PokemonCreator/login.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'username already exists')
+            return redirect("register.html")
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            user.set_password(password)
+            user.save()
+            print("success")
+            return redirect("login.html")
+    else:
+        return render(request, "PokemonCreator/register.html")
+class CreateView(generic.CreateView):
+    template_name = "PokemonCreator/create.html"
+
+class CollectionView(generic.ListView):
+    model = PokemonCard
+    template_name = "PokemonCreator/collection.html"
+
+class DetailView(generic.DetailView):
+    model = PokemonCard
+    template_name = "PokemonCreator/detail.html"
+    context_object_name = "created_pokemon"
+
+    def get_queryset(self):
+        return PokemonCard.objects.filter(creator=User.get_username)
